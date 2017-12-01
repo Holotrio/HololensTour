c2069961aa72ba20558e1195f58b518c7443eb61
kishtrim <markus.kiser@hotmail.com>
2017-12-01 10:17:48 +0100

    Tourbackend Änderung RecoMan mit EmguCv

--- TourBackend/TourBackend/TourBackend .NETStandard/Actors/RecognitionManager.cs
+++ TourBackend/TourBackend/TourBackend .NETStandard/Actors/RecognitionManager.cs
@@ -7,6 +7,9 @@ using Proto;
 using System.Drawing;
 using Emgu;
 using Emgu.CV.Aruco;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
 
 namespace TourBackend
 {
@@ -98,6 +101,7 @@ namespace TourBackend
                     {
                         // do the work here with the recognition of the frame
                         FrameEvaluation(n.bitmap);
+                   
                         // after the successfull evaluation respond to the control Actor
                         var _respondMsg = new RespondNewFrameArrived(n.id);
                         context.Sender.Tell(_respondMsg);
@@ -107,7 +111,6 @@ namespace TourBackend
             return Actor.Done;
         }
 
-
         /// <summary>
         /// the idea here is that we use this function to recognize the markers in the bitmap and update then all
         /// markers in the dictionary with their position and rotation etc. 
@@ -115,12 +118,41 @@ namespace TourBackend
         public void FrameEvaluation(Bitmap _bitmap)
         {
             // use the Function from Emgu.Cv.Aruco.ArucoInvoke for the detection: 
+            // define the frame parameters
+            const int _width = 2160; // Pixelwidth
+            const int _height = 1440; // Pixelheight
+            const float _markerLength = 0.1f; // usually the unit is meter
+            // create the camera Matrix and the distortion coefficients from https://github.com/qian256/HoloLensCamCalib/blob/master/near/hololens896x504.yaml
+            VectorOfFloat _cameraMatrix = new VectorOfFloat();
+            float [] _camMa1 = { 1039.7024546115156f, 0.0f, 401.9889542361556f };
+            float [] _camMa2 = { 0.0f, 1038.5598693279526f, 179.02511993572065f };
+            float[] _camMa3 = { 0.0f, 0.0f, 11.0f };
+            _cameraMatrix.Push(_camMa1);
+            _cameraMatrix.Push(_camMa2);
+            _cameraMatrix.Push(_camMa3);
 
+            float[] _distCo = { 0.1611302127599187f, 0.11645978908419138f, -0.020783847362699993f, -0.006686827095385685f, 0.0f};
+            VectorOfFloat _distortionCoefficients = new VectorOfFloat(_distCo);
+
+            Emgu.CV.Image<Bgr, Byte> _image = Utils.CreateMatfromArray(_bitmap, _width, _height);
+
+            var _outCorners = new VectorOfVectorOfPointF();
+
+            var _outIDs = new VectorOfInt();
+
+            DetectorParameters _detectorParameters = DetectorParameters.GetDefault();
+
+            var _dict = new Dictionary(Dictionary.PredefinedDictionaryName.DictArucoOriginal);
+
+            // now do the evaluation
+            Emgu.CV.Aruco.ArucoInvoke.DetectMarkers(_image, _dict, _outCorners, _outIDs, _detectorParameters, null);
             /*  *** here we should also have the conversion of the information from the corner information to the actual
              * rotation and position of the marker *** */
-
+            VectorOfPoint3D32F _rvecs = new VectorOfPoint3D32F();
+            VectorOfPoint3D32F _tvecs = new VectorOfPoint3D32F();
+            Emgu.CV.Aruco.ArucoInvoke.EstimatePoseSingleMarkers(_outCorners, _markerLength, _cameraMatrix, _distortionCoefficients,  _rvecs,  _tvecs);
             // now we have done the evaluation of the frame and now we want to update the dictionary with the current data
-
+            UpdateInternalDictionary(_outIDs);
         }
 
         /// <summary>
@@ -128,7 +160,7 @@ namespace TourBackend
         /// get the value true for their components isActive. Further the codeObjects should be updated with their
         /// current position and rotation as it was recognised in the FrameEvaluation Method
         /// </summary>
-        public void UpdateInternalDictionary(int[] _ids)
+        public void UpdateInternalDictionary(VectorOfInt _ids)
         {
             // iterate through the whole internal dictionary, to set all CodeObject.isActive default to false
             foreach (var entry in codeObjectIDToCodeObject)
@@ -137,7 +169,7 @@ namespace TourBackend
             }
 
             // idea: go through the whole array of detected markers to update the data in the internal dictionary 
-            for (int i = 0; i < _ids.Length; ++i)
+            for (int i = 0; i < _ids.Size; ++i)
             {
                 if (codeObjectIDToCodeObject.ContainsKey(_ids[i]))
                 {
